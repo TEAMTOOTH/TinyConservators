b6be8026ef05278a647a24a2b6e11ad817d8e481
LudaSeb <sebastian@silverbeans.com>
2025-07-04 15:53:45 +0200

    Did work on animations all of them

--- TinyConservators/Assets/Scripts/Systems/ScrollingSprite.cs
+++ TinyConservators/Assets/Scripts/Systems/ScrollingSprite.cs
@@ -3,20 +3,18 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class ScrollingSprite : MonoBehaviour
 {
-    public Vector2 scrollSpeed = new Vector2(0.5f, 0f); // X: horizontal, Y: vertical
-
+    public Vector2 scrollSpeed = new Vector2(0.5f, 0f);
     private Material material;
     private Vector2 offset;
 
     void Start()
     {
-        // Get a unique instance of the material
         material = GetComponent<SpriteRenderer>().material;
     }
 
     void Update()
     {
         offset += scrollSpeed * Time.deltaTime;
-        material.mainTextureOffset = offset;
+        material.SetVector("_Offset", offset);
     }
 }
